0983902eeb5e3b151af50f1c5a4e5eaeb713a9ab
Nyame-Wolf <mumenyam@gmail.com>
2022-11-21 23:29:46 +0300

    Configure webpack
    add react to application.js and create default view with id root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>);
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'greetings#index'
+  get 'greetings/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
