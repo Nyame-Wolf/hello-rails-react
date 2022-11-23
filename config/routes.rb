302e925d21d4c4ad3ca65da0fed68d5b583c75b9
Nyame-Wolf <mumenyam@gmail.com>
2022-11-23 20:10:44 +0300

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'greetings#index'
-  get 'greetings/index'
+  get 'greetings/api'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
