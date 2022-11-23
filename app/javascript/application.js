790b68690dd2ec948805bbb76b89e72542999216
Nyame-Wolf <mumenyam@gmail.com>
2022-11-23 20:14:45 +0300

    set up store and redux reducers and actions and adding them to the app
    fix greeting component to use redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,8 +4,19 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './redux/store'
+
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
-console.log("hello")
+root.render(
+  <React.StrictMode>
+     <BrowserRouter>
+     <Provider store={store}>
+        <App/>
+     </Provider> 
+     </BrowserRouter>
+  </React.StrictMode>,
+);
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Routes, Route} from "react-router-dom"
 import Greeting from "./Greeting";
 
 function App ()  {
 return (
-<BrowserRouter>
     <Routes>
         <Route path="/" element={<Greeting />} />
     </Routes>
-</BrowserRouter>
 );
 }
 export default App;
